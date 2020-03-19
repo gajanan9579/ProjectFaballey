292085cd01738d5df33f9612757fcc641e7b0787
Pramod <Pramod@LAPTOP-O8D1T5RF>
2020-03-20 00:10:51 +0530

    Third commit added class App2

---
+++ src/test/java/com/alsis/App2.java
@@ -0,0 +1,14 @@
+package com.alsis;
+
+import org.testng.annotations.Test;
+
+import com.alsis.testbase.TestBase;
+
+public class App2 extends TestBase{
+	
+	@Test
+	public void m3(){
+		System.out.println(driver.getPageSource());
+	}
+
+}
