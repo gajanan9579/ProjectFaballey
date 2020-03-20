dec35c4b7e99d3ce93243894d758dbe04aa8f301
Team Leader For Vaibhav Ghule..... <59682415+Vaibhav1430@users.noreply.github.com>
2020-03-20 17:41:30 +0530

    my first commit added utility Comparetext class. and test case in homepage and added elements in homepageor

--- src/main/java/com/alsis/base/Base.java
+++ src/main/java/com/alsis/base/Base.java
@@ -28,7 +28,7 @@ public class Base {
 
 			// log4j
 			log = Logger.getLogger(Base.class);
-			PropertyConfigurator.configure("C:\\workspaceProject\\ProjectFaballey\\src\\resources\\java\\com\\alsis\\propertiesfile\\log4j.properties");
+			PropertyConfigurator.configure("C:\\workspaceProject\\ProjectFaballey\\src\\resources\\java\\com\\alsis\\propertiesfile\\log.properties");
 
 			log.info("currDir is : " + currDir);
 
